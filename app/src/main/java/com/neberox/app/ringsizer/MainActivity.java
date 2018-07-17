ad3187126b4c2e208ec4856321510f517a0a2cd5
MuhammadUmar <ee_umar@yahoo.com>
2018-07-17 20:01:40 +0500

    initial commit

--- app/src/main/java/com/neberox/app/ringsizer/MainActivity.java
+++ app/src/main/java/com/neberox/app/ringsizer/MainActivity.java
@@ -3,14 +3,21 @@ package com.neberox.app.ringsizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.neberox.lib.ringsizer.RingSizer;
+
 public class MainActivity extends AppCompatActivity
 {
+    private RingSizer mRingSizer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRingSizer = (RingSizer) findViewById(R.id.ringSizer);
+        mRingSizer.setDiameter(9.91f);
 
     }
 }
